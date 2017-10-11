6541a293362499d356090261ec36e223788f60d2
spencermendoza <spencerjmendoza@gmail.com>
2017-10-11 11:42:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function donate5 () {
+	alert("Thanks for your (imaginary) $5 donation!");
+}
+
+function donate10 () {
+	alert("Thanks for your (imaginary) $10 donation!");
+}
+
+function donate20 () {
+	alert("Thanks for your (imaginary) $20 donation!");
+}
+
+function donateOwn () {
+	alert("Thanks for your own personal (imaginary) donation!");
+}
